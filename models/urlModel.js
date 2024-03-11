a9b34c56a3601c2b2ceef2dc443edcbe45a0e07b
Munir Abdullahi <abdullahimunir3@gmail.com>
2024-03-11 22:07:49 +0100

    :feat: created url schema

---
+++ models/urlModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+const UrlSchema = mongoose.Schema({
+  urlId: {
+    type: String,
+    required: true,
+  },
+  clicks: {
+    type: number,
+    required: true,
+    default: 0,
+  },
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true
+  },
+  date:{
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model("Url", UrlSchemaSchema)
